be76ad4a07b3770827030a27e15ab81c47211da8
cokfx <av230267@ya.ru>
2020-04-17 09:10:26 +0300

    Core Ready

---
+++ config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'dsn' => 'mysql:host=localhost;dbname=my_project;charset=utf8',
+    'user' => 'root',
+    'pass' => '',
+];
---
+++ vendor/ishop/core/Registry.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ishop;
+
+class Registry {
+
+    use TSingletone;
+//create object of Registry with Arrays of params and properties methods
+    protected static $properties = [];
+
+    public function setProperty($name, $value){
+        self::$properties[$name] = $value;
+    }
+
+    public function getProperty($name){
+        if(isset(self::$properties[$name])){
+            return self::$properties[$name];
+        }
+        return null;
+    }
+
+    public function getProperties(){
+        return self::$properties;
+    }
+
+}
